ac6d3d832a615737e13bd2fc888f74355f0e00cb
abdulhadi-selise <abdulhadi@selise.ch>
2025-01-13 21:25:28 +0600

    variables of go

---
+++ Basic Of Go/variable.go
@@ -0,0 +1,48 @@
+package main
+
+func main() {
+
+	// integer type - int / int8 / int16 / int32 / int64
+	// var num int = 100 // declare a new variable
+	// num = 102         // assign new value
+
+	// name := "Dolly" // With the := sign variable declare
+
+	// var hour = "12pm" //type is inferred
+
+	// var age int //declare a variable without an initial value
+	// var isGoal bool
+	// var course string
+
+	// fmt.Println(isGoal)
+	// fmt.Println(course)
+
+	// fmt.Println("hello ", num)
+	// fmt.Println("hello ", name)
+	// fmt.Println("hello ", hour)
+	// fmt.Println("hello ", age)
+
+	// var a, b, c int = 2, 3, 8
+
+	// var a, b, c = 34, 67, 80
+
+	// a, b, c := 45, 78, 90
+
+	// var (
+	// 	a int
+	// 	b int    = 9
+	// 	c string = "Hellow"
+	// )
+
+	// var a, b, c int
+
+	// const PI float32 = 3.1416
+
+	// fmt.Println(PI)
+
+	// Multiple Constants Declaration
+	// const (
+	// 	PI string = "HH"
+	// 	TT int    = 99
+	// )
+}
