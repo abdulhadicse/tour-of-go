b2313841c9f4ac24ba254a528dd43663cca3c287
abdulhadi-selise <abdulhadi@selise.ch>
2025-01-15 11:56:35 +0600

    loop

---
+++ Basic Of Go/loop.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// i := 4
+	// for i < 6; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// i := 4
+	// for i < 6 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+}
