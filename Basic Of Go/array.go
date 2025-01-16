c9d27c80c70c9adeaa1b527c9b2ef6fbf4641fd0
abdulhadi-selise <abdulhadi@selise.ch>
2025-01-16 11:49:08 +0600

    basic array

---
+++ Basic Of Go/array.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	// declaration array
+	// var myArray [5]int
+
+	//Initialization
+	// var myArray [5]int = [5]int{72, 44, 55, 66, 5}
+	// myArray := [4]string{"Jamal", "kamal", "Rokon", "Alfaz"}
+	// myArray := [...]int{30, 40, 50, 80}
+
+	//Accessing element
+	// myArray := [5]string{"Hello", "World", "Doggy", "Buddy", "Kitty"}
+
+	//Iterating over array
+	// for i, v := range myArray {
+	// 	fmt.Println("Index: ", i, " Value: ", v)
+	// }
+
+	//modifying elements
+	// myArray := [4]string{"A", "C", "F", "G"}
+	// myArray[2] = "YT"
+
+	// fmt.Println("Hello Array")
+	// fmt.Println(myArray)
+	// fmt.Println(myArray[0])
+	// fmt.Println(myArray[2:4])
+}
