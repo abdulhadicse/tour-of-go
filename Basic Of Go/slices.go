f57df96c0818f515b4534d39944c29e367974994
abdulhadi-selise <abdulhadi@selise.ch>
2025-01-16 12:42:41 +0600

    basic slice

---
+++ Basic Of Go/slices.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Slice")
+	// creating slices and initialization
+	// var mySlice []int
+	// mySlice = []int{30, 40, 60}
+	// mySlice := []int{4, 500, 730, 430}
+	// mySlice := make([]int, 4)
+
+	//accessing elements
+	// mySlice := []int{50, 80, 50, 70}
+	// fmt.Println(mySlice[3])
+
+	// appending slice
+	// mySlice = append(mySlice, 60, 33)
+	// fmt.Println(mySlice)
+
+	//Iterating over array
+	// for i, v := range mySlice {
+	// 	fmt.Println("Index: ", i, " Value: ", v)
+	// }
+
+	//Copying a slice
+	// newSlice := mySlice
+	// fmt.Println(mySlice)
+	//fmt.Println(newSlice)
+
+	// copySlice := make([]int, len(mySlice))
+	// copy(copySlice, mySlice)
+	// mySlice[2] = 800
+	// fmt.Println(copySlice)
+	// fmt.Println(mySlice)
+}
